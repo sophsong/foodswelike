75be7716f0ce03edda866531701af30fea382ae4
github username <github email address>
2020-05-19 13:41:17 -0400

    initial setup steps for frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header"> FOODS we LIKE
+        <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528"  />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
