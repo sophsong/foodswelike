f92aab6b039ad60faf1fd02516c97231a60cc5eb
github username <github email address>
2020-05-26 09:50:03 -0400

    working on main page aesthetic and functionality

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar'
 import MainContainer from './containers/MainContainer'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
-        <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528"  />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+       <Route exact path='/' component={Home}/>
+        <NavBar />
+       </Router>
     </div>
   );
 }
